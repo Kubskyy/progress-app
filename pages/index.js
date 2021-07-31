26b16c66b2c220e80a052b83ca26628a4aa8fd53
Kuba Rutkowski <kubaa.rutkowski123@gmail.com>
2021-07-31 22:02:25 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Progress</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -16,9 +16,9 @@ export default function Home() {
           </a>
         </h1>
 
-        <p className="mt-3 text-2xl">
+        <p className="mt-6 text-2xl">
           Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
+          <code className="ml-2 p-3 font-mono text-lg bg-gray-300 rounded-md">
             pages/index.js
           </code>
         </p>
